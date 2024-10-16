eabb494e35a4e71a410d1d8d4a1f40f4b4d9d9aa
WanessaT <wanessatlsousa@gmail.com>
2024-10-16 09:58:14 -0300

    feat: desenvolvimento da tela de cadastro do funcionario

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Forms } from "../components/Forms";
 import { RegisterClient } from "../screens/RegisterClient";
 import { RegisterSupplier } from "../screens/RegisterSupplier";
+import { RegisterEmployee } from "../screens/RegisterEmployee";
 export const AppRoutes = () => {
   return (
     <BrowserRouter>
@@ -9,7 +10,7 @@ export const AppRoutes = () => {
       <Route path="/form" element={<Forms />} />
       <Route path="/register-client" element={<RegisterClient />} />
       <Route path="/register-supplier" element={<RegisterSupplier />} />
-
+      <Route path="/register-employee" element={<RegisterEmployee />} />
       </Routes>
     </BrowserRouter>
   );
