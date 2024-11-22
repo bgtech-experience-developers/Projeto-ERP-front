9f31aaf0614d42f0eb1dfb4343bc6d7dfa3574ff
WanessaT <wanessatlsousa@gmail.com>
2024-11-22 15:22:24 -0300

    refactor: detalhes

--- src/components/LogoSidebar/style.jsx
+++ src/components/LogoSidebar/style.jsx
@@ -3,13 +3,14 @@ import styled from "styled-components";
 import { theme } from "../../theme/theme"; 
 
 export const StyledLogo = styled.div`
-    width: 100%;
+    width: 85%;
     height: 10rem;
     border-bottom: 0.1rem solid ${({ theme }) => theme.colors.lightgray3};
     display: flex;
     justify-content: center;
     padding-top: 1rem;
     margin: auto;
+    margin-bottom: 2rem;
     cursor: pointer;
 `
 
