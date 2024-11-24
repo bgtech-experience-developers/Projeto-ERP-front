6b6cf34eab3a79dc4bc0c5b45d8003d5f64e2d05
Carloss-Dev <carlos2019cavalcante@gmail.com>
2024-11-24 19:54:26 -0300

    feat: reatividade dos campos de clientes

--- src/screens/RegisterClients/index.jsx
+++ src/screens/RegisterClients/index.jsx
@@ -8,17 +8,73 @@ import { useState } from "react";
 
 export const RegisterClients = () => {
   const [formValues, setFormValues] = useState({
-    razaoSocial: "",
+    imagens: [true, false, false],
+    cliente: {
+      corporate_reason: "",
+      fantasy_name: "",
+      cnpj: "",
+      state_registration: "",
+      type_contribuition: "",
+      branch_activity: "",
+    },
+    endereco_empresa: {
+      cep: "",
+      street: "",
+      number: "",
+      complement: "",
+      city: "",
+      neighborhood: "",
+    },
+    endereco_entrega: {
+      cep: "",
+      street: "",
+      number: "",
+      complement: "",
+      city: "",
+      neighborhood: "",
+    },
+    financeiro: {
+      name: "",
+      phone: "",
+      cell_phone: "",
+      rg: "",
+      email: "",
+    },
+    comercial: {
+      name: "",
+      phone: "",
+      cell_phone: "",
+      rg: "",
+      email: "",
+    },
+    contabil: {
+      name: "",
+      phone: "",
+      cell_phone: "",
+      rg: "",
+      email: "",
+    },
+    socio: {
+      name: "",
+      phone: "",
+      cell_phone: "",
+      rg: "",
+      email: "",
+    },
   });
 
   const [errors, setErrors] = useState({});
 
   // Função chamada quando o usuário digita nos campos obrigatórios
-  const handleInputChange = (event) => {
+  const handleInputChange = (field) => (event) => {
     const { id, value } = event.target;
+
     setFormValues({
       ...formValues,
-      [id]: value,
+      [field]: {
+        ...formValues[field],
+        [id]: value,
+      },
     });
 
     console.log(formValues);
@@ -69,10 +125,10 @@ export const RegisterClients = () => {
           <FormsField variant="file" align="flex-end">
             <FormsField>
               <Input
-                id="razaoSocial"
+                id="corporate_reason"
                 height="4.8rem"
-                value={formValues.razaoSocial}
-                onChange={handleInputChange}
+                value={formValues.cliente.corporate_reason}
+                onChange={handleInputChange("cliente")}
                 onBlur={handleBlur}
               >
                 Razão social
@@ -89,10 +145,10 @@ export const RegisterClients = () => {
                 )}
               </Input>
               <Input
-                id="nomeFantasia"
+                id="fantasy_name"
                 height="4.8rem"
-                value={formValues.nomeFantasia}
-                onChange={handleInputChange}
+                value={formValues.cliente.fantasy_name}
+                onChange={handleInputChange("cliente")}
                 onBlur={handleBlur}
               >
                 Nome fantasia
@@ -112,7 +168,12 @@ export const RegisterClients = () => {
             <File text="Adicionar foto" />
           </FormsField>
 
-          <Input id="ramoAtuacao" height="4.8rem">
+          <Input
+            id="branch_activity"
+            value={formValues.cliente.branch_activity}
+            onChange={handleInputChange("cliente")}
+            height="4.8rem"
+          >
             Ramo de atuação
           </Input>
 
@@ -120,8 +181,8 @@ export const RegisterClients = () => {
             <Input
               id="cnpj"
               height="4.8rem"
-              value={formValues.cnpj}
-              onChange={handleInputChange}
+              value={formValues.cliente.cnpj}
+              onChange={handleInputChange("cliente")}
               onBlur={handleBlur}
             >
               CNPJ
@@ -137,85 +198,159 @@ export const RegisterClients = () => {
                 </span>
               )}
             </Input>
-            <Input id="inscricaoEstadual" height="4.8rem">
+            <Input
+              id="state_registration"
+              value={formValues.cliente.state_registration}
+              onChange={handleInputChange("cliente")}
+              height="4.8rem"
+            >
               Inscrição estadual
             </Input>
           </FormsField>
 
           <Input
             type="select"
-            id="tipoContribuinte"
+            id="type_contributor"
             height="4.8rem"
             options={[
-              { value: "opcao2", label: "Opção 1" },
-              { value: "opcao3", label: "Opção 2" },
+              { value: "fisíca", label: "Opção 1" },
+              { value: "jurídica", label: "Opção 2" },
             ]}
           >
             Tipo de contribuinte
           </Input>
         </Card>
-
         <Card title="Endereço da Empresa">
           <FormsField variant="triple">
-            <Input id="logradouro" height="4.8rem">
+            <Input
+              id="street"
+              value={formValues.endereco_empresa.street}
+              onChange={handleInputChange("endereco_empresa")}
+              height="4.8rem"
+            >
               Logradouro
             </Input>
-            <Input id="numero" height="4.8rem">
+            <Input
+              id="number"
+              height="4.8rem"
+              value={formValues.endereco_empresa.number}
+              onChange={handleInputChange("endereco_empresa")}
+            >
               Número
             </Input>
-            <Input id="cep" height="4.8rem">
+            <Input
+              id="cep"
+              height="4.8rem"
+              value={formValues.endereco_empresa.cep}
+              onChange={handleInputChange("endereco_empresa")}
+            >
               CEP
             </Input>
           </FormsField>
 
-          <Input id="complemento" height="4.8rem">
+          <Input
+            id="complement"
+            height="4.8rem"
+            value={formValues.endereco_empresa.complement}
+            onChange={handleInputChange("endereco_empresa")}
+          >
             Complemento
           </Input>
 
           <FormsField variant="double">
-            <Input id="bairro" height="4.8rem">
+            <Input
+              id="neighborhood"
+              height="4.8rem"
+              value={formValues.endereco_empresa.neighborhood}
+              onChange={handleInputChange("endereco_empresa")}
+            >
               Bairro
             </Input>
-            <Input id="cidade" height="4.8rem">
+            <Input
+              id="city"
+              height="4.8rem"
+              value={formValues.endereco_empresa.city}
+              onChange={handleInputChange("endereco_empresa")}
+            >
               Cidade
             </Input>
           </FormsField>
         </Card>
-
+        76
         <Card title="Endereço de Entrega">
           <FormsField variant="triple">
-            <Input id="logradouroEntrega" height="4.8rem">
+            <Input
+              id="street"
+              height="4.8rem"
+              value={formValues.endereco_entrega.street}
+              onChange={handleInputChange("endereco_entrega")}
+            >
               Logradouro
             </Input>
-            <Input id="numeroEntrega" height="4.8rem">
+            <Input
+              id="number"
+              height="4.8rem"
+              value={formValues.endereco_entrega.number}
+              onChange={handleInputChange("endereco_entrega")}
+            >
               Número
             </Input>
-            <Input id="cepEntrega" height="4.8rem">
+            <Input
+              id="cep"
+              height="4.8rem"
+              value={formValues.endereco_entrega.cep}
+              onChange={handleInputChange("endereco_entrega")}
+            >
               CEP
             </Input>
           </FormsField>
 
-          <Input id="complementoEntrega" height="4.8rem">
+          <Input
+            id="complement"
+            height="4.8rem"
+            value={formValues.endereco_entrega.complement}
+            onChange={handleInputChange("endereco_entrega")}
+          >
             Complemento
           </Input>
 
           <FormsField variant="double">
-            <Input id="bairroEntrega" height="4.8rem">
+            <Input
+              id="neighborhood"
+              value={formValues.endereco_entrega.neighborhood}
+              onChange={handleInputChange("endereco_entrega")}
+              height="4.8rem"
+            >
               Bairro
             </Input>
-            <Input id="cidadeEntrega" height="4.8rem">
+            <Input
+              id="city"
+              value={formValues.endereco_entrega.city}
+              onChange={handleInputChange("endereco_entrega")}
+              height="4.8rem"
+            >
               Cidade
             </Input>
           </FormsField>
         </Card>
-
         <Card title="Sócio Proprietário">
           <FormsField variant="file" align="flex-end">
             <FormsField>
-              <Input id="nomeSocio" height="4.8rem">
+              <Input
+                id="name"
+                height="4.8rem"
+                value={formValues.socio.name}
+                onChange={handleInputChange("socio")}
+              >
                 Nome
               </Input>
-              <Input id="emailSocio" height="4.8rem" type="email">
+              <Input
+                id="email"
+                height="4.8rem"
+                type="email"
+                value={formValues.socio.email}
+                onChange={handleInputChange("socio")}
+              >
                 Email
               </Input>
             </FormsField>
@@ -223,63 +358,61 @@ export const RegisterClients = () => {
           </FormsField>
 
           <FormsField variant="double">
-            <Input id="telefoneSocio" height="4.8rem">
+            <Input
+              id="phone"
+              height="4.8rem"
+              value={formValues.socio.phone}
+              onChange={handleInputChange("socio")}
+            >
               Telefone
             </Input>
-            <Input id="celularSocio" height="4.8rem">
+            <Input
+              id="cell_phone"
+              height="4.8rem"
+              value={formValues.socio.cell_phone}
+              onChange={handleInputChange("socio")}
+            >
               Celular
             </Input>
           </FormsField>
 
           <FormsField variant="double">
-            <Input id="rgSocio" height="4.8rem">
+            <Input
+              id="rg"
+              height="4.8rem"
+              value={formValues.socio.rg}
+              onChange={handleInputChange("socio")}
+            >
               RG
             </Input>
-            <Input id="cpfSocio" height="4.8rem">
+            <Input
+              id="cpfSocio"
+              height="4.8rem"
+              value={formValues.socio.cpf}
+              onChange={handleInputChange("socio")}
+            >
               CPF
             </Input>
           </FormsField>
         </Card>
-
-        <Card title="Contato Financeiro">
-          <FormsField variant="file" align="flex-end">
-            <FormsField>
-              <Input id="nomeFinanceiro" height="4.8rem">
-                Nome
-              </Input>
-              <Input id="emailFinanceiro" height="4.8rem" type="email">
-                Email
-              </Input>
-            </FormsField>
-            <File text="Adicionar foto" />
-          </FormsField>
-
-          <FormsField variant="double">
-            <Input id="telefoneFinanceiro" height="4.8rem">
-              Telefone
-            </Input>
-            <Input id="celularFinanceiro" height="4.8rem">
-              Celular
-            </Input>
-          </FormsField>
-
-          <FormsField variant="double">
-            <Input id="rgFinanceiro" height="4.8rem">
-              RG
-            </Input>
-            <Input id="cpfFinanceiro" height="4.8rem">
-              CPF
-            </Input>
-          </FormsField>
-        </Card>
-
         <Card title="Contato Comercial">
           <FormsField variant="file" align="flex-end">
             <FormsField>
-              <Input id="nomeComercial" height="4.8rem">
+              <Input
+                id="name"
+                height="4.8rem"
+                value={formValues.comercial.name}
+                onChange={handleInputChange("comercial")}
+              >
                 Nome
               </Input>
-              <Input id="emailComercial" height="4.8rem" type="email">
+              <Input
+                id="email"
+                height="4.8rem"
+                type="email"
+                value={formValues.comercial.email}
+                onChange={handleInputChange("comercial")}
+              >
                 Email
               </Input>
             </FormsField>
@@ -287,31 +420,123 @@ export const RegisterClients = () => {
           </FormsField>
 
           <FormsField variant="double">
-            <Input id="telefoneComercial" height="4.8rem">
+            <Input
+              id="phone"
+              height="4.8rem"
+              value={formValues.comercial.phone}
+              onChange={handleInputChange("comercial")}
+            >
               Telefone
             </Input>
-            <Input id="celularComercial" height="4.8rem">
+            <Input
+              id="cell_phone"
+              height="4.8rem"
+              value={formValues.comercial.cell_phone}
+              onChange={handleInputChange("comercial")}
+            >
               Celular
             </Input>
           </FormsField>
 
           <FormsField variant="double">
-            <Input id="rgComercial" height="4.8rem">
+            <Input
+              id="rg"
+              height="4.8rem"
+              value={formValues.comercial.rg}
+              onChange={handleInputChange("comercial")}
+            >
               RG
             </Input>
-            <Input id="cpfComercial" height="4.8rem">
+            <Input
+              id="cpf"
+              height="4.8rem"
+              value={formValues.comercial.cpf}
+              onChange={handleInputChange("comercial")}
+            >
               CPF
             </Input>
           </FormsField>
         </Card>
+        <Card title="Contato Financeiro">
+          <FormsField variant="file" align="flex-end">
+            <FormsField>
+              <Input
+                id="name"
+                height="4.8rem"
+                value={formValues.financeiro.name}
+                onChange={handleInputChange("financeiro")}
+              >
+                Nome
+              </Input>
+              <Input
+                id="email"
+                height="4.8rem"
+                type="email"
+                value={formValues.financeiro.email}
+                onChange={handleInputChange("financeiro")}
+              >
+                Email
+              </Input>
+            </FormsField>
+            <File text="Adicionar foto" />
+          </FormsField>
 
+          <FormsField variant="double">
+            <Input
+              id="phone"
+              height="4.8rem"
+              value={formValues.financeiro.phone}
+              onChange={handleInputChange("financeiro")}
+            >
+              Telefone
+            </Input>
+            <Input
+              id="cell_phone"
+              height="4.8rem"
+              value={formValues.financeiro.cell_phone}
+              onChange={handleInputChange("financeiro")}
+            >
+              Celular
+            </Input>
+          </FormsField>
+
+          <FormsField variant="double">
+            <Input
+              id="rg"
+              height="4.8rem"
+              value={formValues.financeiro.rg}
+              onChange={handleInputChange("financeiro")}
+            >
+              RG
+            </Input>
+            <Input
+              id="cpf"
+              height="4.8rem"
+              value={formValues.financeiro.cpf}
+              onChange={handleInputChange("financeiro")}
+            >
+              CPF
+            </Input>
+          </FormsField>
+        </Card>
         <Card title="Contato Contábil">
           <FormsField variant="file" align="flex-end">
             <FormsField>
-              <Input id="nomeContabil" height="4.8rem">
+              <Input
+                id="name"
+                height="4.8rem"
+                value={formValues.contabil.name}
+                onChange={handleInputChange("contabil")}
+              >
                 Nome
               </Input>
-              <Input id="emailContabil" height="4.8rem" type="email">
+              <Input
+                id="email"
+                height="4.8rem"
+                type="email"
+                value={formValues.contabil.email}
+                onChange={handleInputChange("contabil")}
+              >
                 Email
               </Input>
             </FormsField>
@@ -319,24 +544,43 @@ export const RegisterClients = () => {
           </FormsField>
 
           <FormsField variant="double">
-            <Input id="telefoneContabil" height="4.8rem">
+            <Input
+              id="phone"
+              height="4.8rem"
+              value={formValues.contabil.phone}
+              onChange={handleInputChange("contabil")}
+            >
               Telefone
             </Input>
-            <Input id="celularContabil" height="4.8rem">
+            <Input
+              id="cell_phone"
+              height="4.8rem"
+              value={formValues.contabil.cell_phone}
+              onChange={handleInputChange("contabil")}
+            >
               Celular
             </Input>
           </FormsField>
 
           <FormsField variant="double">
-            <Input id="rgContabil" height="4.8rem">
+            <Input
+              id="rg"
+              height="4.8rem"
+              value={formValues.contabil.rg}
+              onChange={handleInputChange("contabil")}
+            >
               RG
             </Input>
-            <Input id="cpfContabil" height="4.8rem">
+            <Input
+              id="cpf"
+              height="4.8rem"
+              value={formValues.contabil.cpf}
+              onChange={handleInputChange("contabil")}
+            >
               CPF
             </Input>
           </FormsField>
         </Card>
-
         <Button type="submit">Cadastrar</Button>
       </Form>
     </div>
